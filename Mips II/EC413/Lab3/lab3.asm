514cd6d682271d466a3a27ce2323105a7cef53f4
JiaweiXiang <x1angjw@bu.edu>
2024-02-16 10:10:57 -0500

    save

--- Mips II/EC413/Lab3/lab3.asm
+++ Mips II/EC413/Lab3/lab3.asm
@@ -1,109 +1,168 @@
 ############################################################################
-#                       Lab 3 - Pre-Lab
+#                       Lab 3
 #                       EC413
 #
-#    		Assembly Language Lab -- Addressing.
+#    		Assembly Language Lab -- Programming with Loops.
 #
 ############################################################################
 #  DATA
 ############################################################################
         .data           # Data segment
-Hello:	.asciiz " \n Hello World! \n "  # declare a zero terminated string
-Q1:		.asciiz " \n Q1 Answer: \n"	
-Q2:		.asciiz " \n Q2 Answer: \n"
-In1: .word	5		# Input data for Question 1 
-In2: .word	9		# Input data for Question 1
-In3: .word	7		# Input data for Question 1
-Out1:.word 0		# Output data for Question 1
-In4: .word 4, 7, 9	# Input data for Question 2
-Out2:.word 0, 0, 0	# Output data for Question 2
-space:	.asciiz	" "	# declare a zero terminate string
-
+Hello:  .asciiz " \n Hello World! \n "  # declare a zero terminated string
+Hello_len: .word 16
+AnInt:	.word	12		# a word initialized to 12
+space:	.asciiz	" "	        # declare a zero terminate string
+WordAvg:   .word 0		# use this variable for part 6
+ValidInt:  .word 0		#
+ValidInt2: .word 0		#
+lf:     .byte	10, 0		# string with carriage return and line feed
+InLenW:	.word   4       	# initialize to number of words in input1 and input2
+InLenB:	.word   16      	# initialize to number of bytes in input1 and input2
+        .align  4
+Input1:	.word	0x01020304,0x05060708
+	.word	0x090A0B0C,0x0D0E0F10
+        .align  4
+Input2: .word   0x01221117,0x090b1d1f   # input
+        .word   0x0e1c2a08,0x06040210
+        .align  4
+Copy:  	.space  0x80    # space to copy input word by word
+        .align  4
+ 
+Enter: .asciiz "\n"
+Comma: .asciiz ","
+Empty: .space 12	# put some empty spaces here so the starting addr of Text is clear
+Text: .asciiz " \n It is exciting to watch flying fish after a hard day of work. I do not know why some fish prefer flying and other fish would rather swim. It seems like the fish just woke up one day and decided, Hey, today is the day to fly away \n "	# a big string
 ############################################################################
 #  CODE
 ############################################################################
         .text                   # code segment
 #
-# Print out greetings
+# print out greeting
 #
 main:
-        la	$a0,Hello		# address of string to print
-        li	$v0,4			# system call code for print_str
-        syscall         	# print the string
-		
-#
-# Question 1-- 
-# Use direct addressing to add 3 numbers in memory location In1, In2, In3 and write the result to Out1
-# Print out the value stored in Out1 after calculation
-#
-##############Signify the output of Q1############
-		la	$a0,Q1
-        li	$v0,4	
-        syscall
-		
-##############Your code from here#################
+        la	$a0,Hello	# address of string to print
+        li	$v0,4		# system call code for print_str
+        syscall                 # print the string
 
-        la  $t0,In1
-        lw  $t0,0($t0)
-        la  $t1,In2
-        lw  $t1,0($t1)
-        la  $t2,In3
-        lw  $t2,0($t2)
+	
+#Code for Item 2
+#Count number of occurrences of letter "a" and "e" in Text string and compute the difference between the number of occurrences
 
-        add $t0,$t0,$t1
-        add $t0,$t0,$t2
 
-        la  $t3,Out1
-        sw  $t0,0($t3)
 
-##############Print out Out1######################			
-		lw	$a0,Out1		# Load a value in Out1 to $a0
-		li	$v0,1			# syscall command 1 -- Print out 1 word
-		syscall
-		
-		
-		
-		
-		
+
+
+
+
+
+
+################################################################################
+        la	$a0,Enter	# address of string to print
+        li	$v0,4		# system call code for print_str
+        syscall                 # print the string
+################################################################################
 
 #
-# Question 2--
-# Use indirect addressing to reverse the data sequence in array In4 and store the new sequence in Out2
-# Print out the value stored in Out2 after operation
+# Code for Item 3 -- 
+# Print the integer value of numbers from 5 and less than AnInt
 #
 
-##############Signify the output of Q2############
-		la	$a0,Q2
-        li	$v0,4	
-        syscall
-		
-##############Your code from here#################		
-		
-		la  $t4,In4
-        la  $t5,Out2
 
-        lw  $t6,0($t4)
-        sw  $t6,8($t5)
-        lw  $t6,4($t4)
-        sw  $t6,4($t5)
-        lw  $t6,8($t4)
-        sw  $t6,0($t5)
-				
-		
-##############Print out Out2######################		
-		li	$t6,3
-		la	$t5,Out2		# $t5 point to the beginning of Out2
-Print_Out2:
-		lw	$a0,($t5)		# Load the first value in Out2 to $a0
-		li	$v0,1			# syscall command 1 -- Print out 1 word
-		syscall
-		add $t5,4			# $t5 point to the 2nd element of Out2
-		add $t6,-1			# counter--
-		bgtz $t6,Print_Out2		
-		
-		
-		
-		
-    jr $ra		
-		
-		
+
+
+
+
+
+
+
+###################################################################################
+        la	$a0,Enter	# address of string to print
+        li	$v0,4		# system call code for print_str
+        syscall                 # print the string
+###################################################################################
+#
+# Code for Item 4 -- 
+# Print the integer values of each byte less than 4 in the array Input2 (with spaces)
+#
+
+
+
+
+
+
+
+
+
+
+###################################################################################
+#
+# Code for Item 5 -- 
+# Write code to copy the contents of Input2 to Copy
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################################
+        la	$a0,Enter	# address of string to print
+        li	$v0,4		# system call code for print_str
+        syscall                 # print the string
+###################################################################################
+#
+# Code for Item 6 -- 
+# Print the integer average of squares of the contents of array Input1
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################################
+        la	$a0,Enter	# address of string to print
+        li	$v0,4		# system call code for print_str
+        syscall                 # print the string
+##################################################################################
+#
+# Code for Item 7 -- 
+# Display the first 10 integers that are divisible by either 7 and 13 (with space)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# Code for Item 8 -- 
+# Repeat step 7 but display the integers in 3 lines each with 5 integers (with spaces between the integers)
+# This must be implemented using a nested loop.
+#
+
+Exit:
+	jr $ra
